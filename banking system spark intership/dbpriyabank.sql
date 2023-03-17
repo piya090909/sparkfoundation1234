cf99e79ca740f0f1376a60ae9fdf39e0f5a81864
piya090909 <99525476+piya090909@users.noreply.github.com>
2023-03-17 19:35:00 +0530

    Add files via upload

---
+++ banking system spark intership/dbpriyabank.sql
@@ -0,0 +1,82 @@
+phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 08, 2021 at 06:52 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbpriyabank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE `contactus` (
+  `sr` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `message` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contactus`
+--
+
+INSERT INTO `contactus` (`sr`, `name`, `email`, `message`) VALUES
+(13, 'Rohit koravi', 'rohitm.official404@g', 'What is the python data science course fee?'),
+(14, 'Rohit', 'adef32795@gmail.com', 'This is a demo paregraph!'),
+(15, 'Rohit', 'adef327095@gmail.com', 'This is a para!'),
+(16, '', '', ''),
+(17, '', '', ''),
+(18, '', '', ''),
+(19, 'Rohit Mahajan', 'rdm@gmail.com', 'this is my new contact email');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE `transactions` (
+  `sr` int(11) NOT NULL,
+  `sender` varchar(10) NOT NULL,
+  `receiver` varchar(10) NOT NULL,
+  `amount` bigint(20) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`sr`, `sender`, `receiver`, `amount`, `status`) VALUES
+(1, '7311780855', '5353112532', 200, 'succeed'),
+(2, '', '', 0, 'failed'),
+(3, '', '', 0, 'failed'),
+(4, '', '', 0, 'failed'),
+(5, '', '', 0, 'failed'),
+(6, '7311780855', '5353112532', 100, 'succeed'),
+(7, '7311780855', '5353112532', 0, 'failed'),
+(8, '7311780855', '4001286186', 200, 'succeed'),
+(9, '7311780855', '4001286186', 500, 'succeed'),
+(10, '3917416800', '4001286186', 14000, 'failed'),
+(11, '', '', 0, 'failed'),
+(12, '7311780855', '4001286186', 9950000, 'succeed'),
+(13, '4001286186', '7311780855', 500, 'succeed'),
+(14, '4001286186', '5353112532', 800, 'succeed');
